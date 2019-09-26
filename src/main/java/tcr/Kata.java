4d9de9bf0508dc9ce1d5269f2d10aa3ea11e9d33
Thorsten Brunzendorf <thorsten.brunzendorf@codecentric.de>
2019-09-26 20:24:10 +0200

    new initial test

--- src/main/java/tcr/Kata.java
+++ src/main/java/tcr/Kata.java
@@ -2,20 +2,7 @@ package tcr;
 
 public class Kata {
 
-    private int scoreA = 0;
-    private int scoreB = 0;
-
-    private String scoreMapping[] =
-            new String[]{"love", "15", "30", "40"};
-
-    public String getScore() {
-        return scoreMapping[scoreA]
-                + ":"
-                + scoreMapping[scoreB];
-    }
-
-    public void pointForA() {
-        scoreA++;
+    public String doSomething() {
+        return "";
     }
 }
-
--- src/test/java/tcr/KataTest.java
+++ src/test/java/tcr/KataTest.java
@@ -1,33 +1,13 @@
 package tcr;
 
-import org.junit.Ignore;
 import org.junit.Test;
-import static org.junit.Assert.*;
+
+import static org.junit.Assert.assertEquals;
 
 public class KataTest {
-    @Test public void initialScore() {
+    @Test
+    public void initialScore() {
         Kata classUnderTest = new Kata();
-        assertEquals("love:love", classUnderTest.getScore());
-    }
-
-    @Test public void firstPointForA() {
-        Kata classUnderTest = new Kata();
-        classUnderTest.pointForA();
-        assertEquals("15:love", classUnderTest.getScore());
-    }
-
-    @Test public void secondPointForA() {
-        Kata classUnderTest = new Kata();
-        classUnderTest.pointForA();
-        classUnderTest.pointForA();
-        assertEquals("30:love", classUnderTest.getScore());
-    }
-
-    @Test public void thirdPointForA() {
-        Kata classUnderTest = new Kata();
-        classUnderTest.pointForA();
-        classUnderTest.pointForA();
-        classUnderTest.pointForA();
-        assertEquals("40:love", classUnderTest.getScore());
+        assertEquals("", classUnderTest.doSomething());
     }
 }
