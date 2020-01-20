de4cc310d505f525c79ae69f5df41f88cb670bcd
Thorsten Brunzendorf <thorsten.brunzendorf@codecentric.de>
2020-01-20 22:57:38 +0100

    add mockito and assertj

--- src/test/java/tcr/KataTest.java
+++ src/test/java/tcr/KataTest.java
@@ -2,12 +2,12 @@ package tcr;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class KataTest {
     @Test
     public void initialScore() {
         Kata classUnderTest = new Kata();
-        assertEquals("", classUnderTest.doSomething());
+        assertThat(classUnderTest.doSomething()).isEqualTo("");
     }
 }
